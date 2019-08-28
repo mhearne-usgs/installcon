9aac6b2124d8b426987db0984994852f7f5febfe
Michael Hearne <mhearne@usgs.gov>
2019-08-28 16:35:32 -0600

    removed versioneer

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
-import versioneer
-
 
 setup(name='installcon',
       description='Install conda binaries',
       author='Mike Hearne',
       author_email='mhearne@usgs.gov',
       url='https://github.com/mhearne-usgs/installcon',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
       packages=['installcon'],
       entry_points={
           'console_scripts': [
